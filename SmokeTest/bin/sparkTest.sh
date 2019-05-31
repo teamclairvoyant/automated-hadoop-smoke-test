0dcea6fd6d2f5aad7d7ac7a17adb27aeda13efae
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-05-31 17:35:16 +0530

    spark code updated

--- SmokeTest/bin/sparkTest.sh
+++ SmokeTest/bin/sparkTest.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 source ./conf/SmokeConfig.config
 
-echo "this is the end. the only end. my friend." >$SPARK_IN_LOC 
+
+
+
+hdfs dfs -mkdir -p $SPARK_IN_CLUS
+hdfs dfs -rm -r $SPARK_OUT_CLUS
+
+
+echo "this is the end. the only end. my friend." >$SPARK_IN_LOC
 rc=$?; if [[ $rc != 0 ]]; then echo "Can not produce input data! exiting"; echo " - Spark	- Failed [Can not produce input data]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-hdfs dfs -put $SPARK_IN_LOC $SPARK_IN_CLUS
+hdfs dfs -put -f $SPARK_IN_LOC $SPARK_IN_CLUS
 rc=$?; if [[ $rc != 0 ]]; then echo "Can not copy input data! exiting"; echo " - Spark	- Failed [Can not copy input data]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo "--- piEstimation ---"
@@ -19,4 +26,4 @@ echo "**************************************"
 echo "* Spark test completed Successfully! *"
 echo "**************************************"
 
-echo "- Spark	- Passed" >> ./log/SummaryReport.txt
+echo " - Spark	- Passed" >> ./log/SummaryReport.txt
