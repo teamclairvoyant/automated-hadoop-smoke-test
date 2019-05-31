9c4184db260d9edb7804f1301806eb3392aeb7ab
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-05-31 17:49:09 +0530

    add security condition

--- SmokeTest/bin/hivedrop.sh
+++ SmokeTest/bin/hivedrop.sh
@@ -6,5 +6,10 @@ HIVESERVER2=$HIVESERVER2
 REALM=${REALM}
 
 BKOPTS=$BKOPTS
+BTOPTS=$BTOPTS
 
-beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}" -e "DROP TABLE test;"
+if $SECURITY_HIVE ; then
+	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}${BTOPTS}" -e "DROP TABLE test;"
+else
+	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/" -e "DROP TABLE test;"
+fi
