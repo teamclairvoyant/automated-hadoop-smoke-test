a6bd73501eec1edd0f642246cf6abd5aa16ec094
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-05-31 18:05:46 +0530

    Script updated

--- SmokeTest/bin/spark2Test.sh
+++ SmokeTest/bin/spark2Test.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
 source ./conf/SmokeConfig.config
 
-echo "this is the end. the only end. my friend." >$SPARK_IN_LOC 
+hdfs dfs -mkdir -p $SPARK_IN_CLUS
+hdfs dfs -rm -r $SPARK_OUT_CLUS
+
+echo "this is the end. the only end. my friend." >$SPARK_IN_LOC
 rc=$?; if [[ $rc != 0 ]]; then echo "Can not produce input data! exiting"; echo " - Spark2	- Failed [Can not produce input data]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-hdfs dfs -put $SPARK_IN_LOC $SPARK_IN_CLUS
+hdfs dfs -put -f $SPARK_IN_LOC $SPARK_IN_CLUS
 rc=$?; if [[ $rc != 0 ]]; then echo "Can not copy input data! exiting"; echo " - Spark2	- Failed [Can not copy input data]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo "--- piEstimation ---"
@@ -16,6 +20,6 @@ rc=$?; if [[ $rc != 0 ]]; then echo "Word count test failed! exiting"; echo " - 
 
 echo "***************************************"
 echo "* Spark2 test completed Successfully! *"
-echo "***************************************" 
+echo "***************************************"
 
-echo "- Spark2	- Passed" >> ./log/SummaryReport.txt
+echo " - Spark2	- Passed" >> ./log/SummaryReport.txt
