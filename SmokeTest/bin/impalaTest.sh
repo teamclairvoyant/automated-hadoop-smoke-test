34ce26a4390bf895421ab5128eabe9ae1bdb5d7f
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-05-31 17:38:08 +0530

    spark code updated

--- SmokeTest/bin/impalaTest.sh
+++ SmokeTest/bin/impalaTest.sh
@@ -1,13 +1,49 @@
 #!/usr/bin/env bash
+source ./conf/SmokeConfig.config
 
-impala-shell -i  quickstart.cloudera  -q "invalidate metadata;"
-rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; echo " - Impala	- Failed [Invalidation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-impala-shell -i  quickstart.cloudera  -q "select * FROM test;"
-rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; echo " - Impala	- Failed [Select query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-echo "***************************************"
-echo "* Impala test completed Successfully! *"
-echo "***************************************" 
+if $HIVE ; then
 
-echo "- Impala	- Passed" >> ./log/SummaryReport.txt
+    bash ./bin/hivedrop.sh
+    bash ./bin/hiveImpalaTest.sh
+
+
+
+    impala-shell -i  $IMPALADAEMON -q "invalidate metadata;"
+    rc=$?; if [[ $rc != 0 ]]; then echo "Invalidation failed! exiting"; echo " - Impala	- Failed [Invalidation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+
+    impala-shell -i  $IMPALADAEMON -q "select * FROM test;" | tail -n +3 | sed -r 's/[-|+]+/ /g' | awk '{$1=$1};1' > $IMPALA_VAL
+    rc=$?; if [[ $rc != 0 ]]; then echo "Select query failed! exiting"; echo " - Impala	- Failed [Select query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+
+
+
+    export HIVE_SKIP_SPARK_ASSEMBLY=true;
+    hive -S -e "select * FROM test;" | sed -r 's/\t/ /g' > $IMPALA_HIVE
+    diff -B $IMPALA_HIVE $IMPALA_VAL
+    status=$?
+    if [[ $status = 0 ]]; then
+        echo "Files are the same"
+        echo "***************************************"
+	    echo "* Impala test completed Successfully! *"
+	    echo "***************************************"
+
+	    echo " - Impala	- Passed" >> ./log/SummaryReport.txt
+    else
+        echo "Files are different"
+        echo "***************************************"
+	    echo "* Impala test not  completed Successfully! *"
+	    echo "***************************************"
+
+	    echo " - Impala	- Failed[datas in impala and hive are different]" >> ./log/SummaryReport.txt
+    fi
+
+else
+
+	echo "***************************************"
+	echo "* Impala test not  completed Successfully! *"
+	echo "***************************************"
+
+fi
+
+
