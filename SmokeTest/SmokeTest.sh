7718a331ee17a86115c5c58a7752deda045712cd
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:22:01 +0530

    Script updated

--- SmokeTest/SmokeTest.sh
+++ SmokeTest/SmokeTest.sh
@@ -11,85 +11,69 @@ echo "">./log/SummaryReport.txt
 exec &> >(tee -a $LOG_PATH/${timestamp}logs.log)
 
 if $SECURITY ; then
-
-    kinit -kt $KRB_KEYTAB $KRB_PRINCIPAL
-
+	kinit -kt $KRB_KEYTAB $KRB_PRINCIPAL
 fi
 
 echo "|-- $CLUSTER"
 
 if $HDFS ; then
-
-  	echo "Smoke test for HDFS in $CLUSTER"
- 	bash ./bin/hdfsTest.sh
+	echo "Smoke test for HDFS in $CLUSTER"
+	bash ./bin/hdfsTest.sh
 
 fi
 
 if $MAPREDUCE ; then
-
 	echo "Smoke test for MAPREDUCE in $CLUSTER"
-    bash ./bin/yarnTest.sh
+	bash ./bin/yarnTest.sh
 
 fi
 
 if $HIVE ; then
-
 	echo "Smoke test for HIVE in $CLUSTER"
 	bash ./bin/hiveTest.sh
 
 fi
 
 if $HBASE ; then
-
 	echo "Smoke test for HBASE in $CLUSTER"
 	bash ./bin/hbaseTest.sh
 
 fi
 
 if $PIG ; then
-
 	echo "Smoke test for PIG in $CLUSTER"
 	bash ./bin/pigTest.sh
-
 fi
 
 if $IMPALA ; then
-
 	echo "Smoke test for IMPALA in $CLUSTER"
 	bash ./bin/impalaTest.sh
-
 fi
 
 if $SPARK2 ; then
-
- 	echo "Smoke test for SPARK in $CLUSTER"
+	echo "Smoke test for SPARK in $CLUSTER"
 	bash ./bin/spark2Test.sh
-
 fi
 
 if $SPARK ; then
-
-    echo "Smoke test for SPARK in $CLUSTER"
-    bash ./bin/sparkTest.sh
+	echo "Smoke test for SPARK in $CLUSTER"
+	bash ./bin/sparkTest.sh
 
 fi
 
 if $SOLR ; then
-
-    echo "Smoke test for SOLR in $CLUSTER"
-    bash ./bin/solrTest.sh
+	echo "Smoke test for SOLR in $CLUSTER"
+	bash ./bin/solrTest.sh
 
 fi
 
 if $KUDU ; then
-
-    echo "Smoke test for KUDU in $CLUSTER"
-    bash ./bin/kuduTest.sh
+	echo "Smoke test for KUDU in $CLUSTER"
+	bash ./bin/kuduTest.sh
 
 fi
 
 if $KAFKA ; then
-
 	echo "Smoke test for KAFKA in $CLUSTER"
 	bash ./bin/kafkaTest.sh
 
