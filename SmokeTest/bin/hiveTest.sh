086ba2e47f22ed080ddbab6f28d9dbfbf4940c83
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:43:50 +0530

    Script updated

--- SmokeTest/bin/hiveTest.sh
+++ SmokeTest/bin/hiveTest.sh
@@ -12,25 +12,16 @@ if $SECURITY_HIVE ; then
 
 	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}${BTOPTS}" -e "CREATE TABLE test(id INT, name STRING) ROW FORMAT DELIMITED FIELDSTERMINATED BY ' ' STORED AS TEXTFILE;"
 	rc=$?; if [[ $rc != 0 ]]; then echo "Create query failed! exiting"; echo " - Hive	- Failed [Create query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
-
 	echo "1 justin" > $HIVE_DATA_PATH
 	rc=$?; if [[ $rc != 0 ]]; then echo "Input data generation failed! exiting"; echo " - Hive		- Failed [Input data generation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 	echo "2 michael" >> $HIVE_DATA_PATH
 	rc=$?; if [[ $rc != 0 ]]; then echo "Input data generation failed! exiting"; echo " - Hive		- Failed [Input data generation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
-
 	hdfs dfs -put $HIVE_DATA_PATH  $HIVE_TABLE_LOC
 	rc=$?; if [[ $rc != 0 ]]; then echo "Input data transfer failed! exiting"; echo " - Hive		- Failed [Input data transfer failed]" >> ./log/SummaryReport.txt; exit $rc; fi
-
-
 	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}${BTOPTS}" -e"SELECT * FROM test WHERE id=1;"
-
 	rc=$?; if [[ $rc != 0 ]]; then echo "Select query failed! exiting"; echo " - Hive		- Failed [Select query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-	echo "**************************************"
-	echo "* Hive test completed Successfully ! *"
-	echo "**************************************"
 
-	echo " - Hive		- Passed" >> ./log/SummaryReport.txt
 
 else
 
@@ -49,29 +40,26 @@ else
 
 	hdfs dfs -mkdir -p $HIVE_OUT
 
-    echo $HIVE_OUT/${HIVE_DATA_PATH##/*/} $HIVE_DATA_PATH
+	echo $HIVE_OUT/${HIVE_DATA_PATH##/*/} $HIVE_DATA_PATH
 	beeline --showHeader=false --outputformat=tsv2  -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000" -e "SELECT * FROM test WHERE id=1;" > $HIVE_OUT
 	rc=$?; if [[ $rc != 0 ]]; then echo "Select query failed! exiting"; echo " - Hive		- Failed [Select query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 
-
-
-    if grep -f $HIVE_OUT $HIVE_DATA_PATH
-    then
-	    echo "same data as in the output location"
-	    echo "**************************************"
-	    echo "* Hive test completed Successfully ! *"
-	    echo "**************************************"
-
-	    echo " - Hive		- Passed" >> ./log/SummaryReport.txt
-    else
-	    echo "Not same data as in the output location"
-	    echo "**************************************"
-	    echo "* Hive test not  completed Successfully ! *"
-	    echo "**************************************"
-
-	    echo " - Hive		- Failed[Not same data as in the output location]" >> ./log/SummaryReport.txt
-    fi
-
-
 fi
+
+if grep -f $HIVE_OUT $HIVE_DATA_PATH
+then
+	echo "same data as in the output location"
+	echo "**************************************"
+	echo "* Hive test completed Successfully ! *"
+	echo "**************************************"
+	echo " - Hive		- Passed" >> ./log/SummaryReport.txt
+else
+	echo "Not same data as in the output location"
+	echo "**************************************"
+	echo "* Hive test not  completed Successfully ! *"
+	echo "**************************************"
+	echo " - Hive		- Failed[Not same data as in the output location]" >> ./log/SummaryReport.txt
+fi
+
+
