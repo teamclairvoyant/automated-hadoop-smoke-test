cc20ad0e7dbee019d4153a16f2f49dea64595f1f
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 18:28:25 +0530

    config file updated

--- SmokeTest/bin/hivedrop.sh
+++ SmokeTest/bin/hivedrop.sh
@@ -7,6 +7,7 @@ REALM=${REALM}
 
 BKOPTS=$BKOPTS
 
+rm -r -f  $HIVE_TABLE_LOC
 if $SECURITY_HIVE ; then
 	kinit -kt $KRB_KEYTAB_HIVE $KRB_PRINCIPAL_HIVE
 	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}${BTOPTS}" -e "DROP TABLE test;"
