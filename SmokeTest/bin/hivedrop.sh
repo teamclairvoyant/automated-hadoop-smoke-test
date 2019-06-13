54bb322108bf67ba300bac1c531951802d28817c
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:38:02 +0530

    Script updated

--- SmokeTest/bin/hivedrop.sh
+++ SmokeTest/bin/hivedrop.sh
@@ -6,9 +6,9 @@ HIVESERVER2=$HIVESERVER2
 REALM=${REALM}
 
 BKOPTS=$BKOPTS
-BTOPTS=$BTOPTS
 
 if $SECURITY_HIVE ; then
+	kinit -kt $KRB_KEYTAB_HIVE $KRB_PRINCIPAL_HIVE
 	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/${BKOPTS}${BTOPTS}" -e "DROP TABLE test;"
 else
 	beeline -n `whoami` -u "jdbc:hive2://${HIVESERVER2}:10000/" -e "DROP TABLE test;"
