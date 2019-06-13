1cb40981d28357e128c3795de65767ee645fb49d
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:37:02 +0530

    Script updated

--- SmokeTest/bin/hdfsTest.sh
+++ SmokeTest/bin/hdfsTest.sh
@@ -2,14 +2,7 @@
 source ./conf/SmokeConfig.config
 
 
-
-echo $HDFS_PATH
-echo $HDFS_PATH_TEMP
 hdfs dfs -mkdir -p $HDFS_PATH
-
-#HDFS="/HDFSOut"
-#HDFS_PATH_TEMP ="$HDFS_PATH$HDFS"
-
 
 hdfs dfs -ls /
 rc=$?; if [[ $rc != 0 ]]; then echo "Error in showing files in HDFS! exiting"; echo " - HDFS		- Failed [Error in showing files in HDFS]" >> ./log/SummaryReport.txt; exit $rc; fi
@@ -25,23 +18,21 @@ rc=$?; if [[ $rc != 0 ]]; then echo "Error in showing copied file from HDFS! exi
 
 
 
-echo  $LOC_PATH $TEMP_PATH/${LOC_PATH##/*/}
-
 cmp $LOC_PATH $TEMP_PATH/${LOC_PATH##/*/}
 status=$?
 if [[ $status = 0 ]]; then
     echo "Files are the same"
     echo " - HDFS		- Passed" >> ./log/SummaryReport.txt
     echo "**************************************"
-    echo "* HDFS test completed Successfully ! *"
-    echo "**************************************"
+	echo "* HDFS test completed Successfully ! *"
+	echo "**************************************"
 
 else
     echo "Files are different"
     echo " - HDFS		- Failed[Files are different]" >> ./log/SummaryReport.txt
     echo "**************************************"
-    echo "* HDFS test not completed Successfully ! *"
-    echo "**************************************"
+	echo "* HDFS test not completed Successfully ! *"
+	echo "**************************************"
 
 fi
 
