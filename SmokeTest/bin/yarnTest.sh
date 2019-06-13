959bb9f94575c8fe7e82d9618483595a8f9e7958
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:58:01 +0530

    Script updated

--- SmokeTest/bin/yarnTest.sh
+++ SmokeTest/bin/yarnTest.sh
@@ -1,5 +1,9 @@
 source ./conf/SmokeConfig.config
 
+
+hdfs dfs -mkdir -p $MAP_REDUCE_IN
+hdfs dfs -mkdir -p $MAP_REDUCE_OUT
+
 hdfs dfs -put -f ./lib/WordCountFile.txt $MAP_REDUCE_IN/
 rc=$?; if [[ $rc != 0 ]]; then echo "Input dat tranfser failed! exiting"; echo " - MapReduce	- Failed [Input dat tranfser failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
@@ -9,11 +13,10 @@ yarn jar ./lib/MapReduce-1.0.jar com.fs.WordCount $MAP_REDUCE_IN/WordCountFile.t
 rc=$?; if [[ $rc != 0 ]]; then echo "Mapreduce Job failed! exiting"; echo " - MapReduce	- Failed [Wordcount test failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 hdfs dfs -rm -r $MAP_REDUCE_IN/WordCountFile.txt
-rc=$?; if [[ $rc != 0 ]]; then echo "Wordcount input removal failed! exiting"; echo " - MapReduce	- Failed [Wordcount input removal failed] >> ./log/SummaryReport.txt; exit $rc; fi
-
+#rc=$?; if [[ $rc != 0 ]]; then echo "Wordcount input removal failed! exiting"; echo " - MapReduce	- Failed [Wordcount input removal failed] >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo "******************************************"
 echo "* MapReduce test completed Successfully! *"
 echo "******************************************"
 
-echo "- MapReduce	- Passed" >> ./log/SummaryReport.txt
+echo " - MapReduce	- Passed" >> ./log/SummaryReport.txt
