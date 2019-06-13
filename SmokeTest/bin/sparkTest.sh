394b6c74c11c601a5cb1e681c662ec6492c07f79
kiran.vincent <kiran.vincent@clairvoyantsoft.com>
2019-06-13 15:57:27 +0530

    Script updated

--- SmokeTest/bin/sparkTest.sh
+++ SmokeTest/bin/sparkTest.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 source ./conf/SmokeConfig.config
 
-
-
-
 hdfs dfs -mkdir -p $SPARK_IN_CLUS
 hdfs dfs -rm -r $SPARK_OUT_CLUS
-
 
 echo "this is the end. the only end. my friend." >$SPARK_IN_LOC
 rc=$?; if [[ $rc != 0 ]]; then echo "Can not produce input data! exiting"; echo " - Spark	- Failed [Can not produce input data]" >> ./log/SummaryReport.txt; exit $rc; fi
