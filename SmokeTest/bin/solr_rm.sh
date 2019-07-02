6e7d7db5b219d3b21daed9e0b21f476fa3369245
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-02 11:18:58 -0700

    removed comments

--- SmokeTest/bin/solr_rm.sh
+++ SmokeTest/bin/solr_rm.sh
@@ -8,8 +8,6 @@ rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
 solrctl instancedir --delete test_config
 rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
 
-#kinit solr
-#hdfs dfs -rm -R -skipTrash /solr/$SOLR_COLLECTION_NAME
 rm -rf /tmp/test_config.$$
 rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
 
