52957a3b50416f4993aa407fc77b68fb838331ba
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-02 14:56:44 -0700

    solr configs

--- SmokeTest/bin/solrTest.sh
+++ SmokeTest/bin/solrTest.sh
@@ -11,10 +11,10 @@ rc=$?; if [[ $rc != 0 ]]; then echo "Config file creation failed! exiting"; echo
 solrctl collection --create $SOLR_COLLECTION_NAME -s 1 -c test_config
 rc=$?; if [[ $rc != 0 ]]; then echo "Collection creation failed! exiting"; echo " - Solr		- Failed [Collection creation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-java -Durl=${STPROTO:-http}://${SOLRSERVER}/solr/$SOLR_COLLECTION_NAME/update -jar $SOLR_EXAMPLE_HOME/exampledocs/post.jar $SOLR_EXAMPLE_HOME/exampledocs/*.xml
+java -Durl=${SOLR_PROTOCOL:-http}://${SOLR_SERVER}/solr/$SOLR_COLLECTION_NAME/update -jar $SOLR_EXAMPLE_HOME/exampledocs/post.jar $SOLR_EXAMPLE_HOME/exampledocs/*.xml
 rc=$?; if [[ $rc != 0 ]]; then echo "durl command failed! exiting"; echo " - Solr		-  Failed [durl command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-curl $SKOPTS "${STPROTO:-http}://${SOLRSERVER}/solr/${SOLR_COLLECTION_NAME}_shard1_replica1/select?q=*%3A*&wt=json&indent=true"
+curl $SKOPTS "${SOLR_PROTOCOL:-http}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}_shard1_replica1/select?q=*%3A*&wt=json&indent=true"
 rc=$?; if [[ $rc != 0 ]]; then echo "curl command failed! exiting"; echo " - Solr		-  Failed [curl command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo ""
