0d1f728cccbad3484da821ae6663ea32ebdb88d1
Robert Sanders <robert.sanders@etg.dom>
2019-07-03 13:22:10 -0700

    Rough changes to:  - echo variables used during run  - move hdfs mkdir command after ls command  - reduce duplicate code in hiveTest.sh  - Remove CLUSTER variable

--- SmokeTest/bin/hiveTest.sh
+++ SmokeTest/bin/hiveTest.sh
@@ -1,50 +1,16 @@
 #!/bin/bash
 source ./conf/SmokeConfig.config
 
+BEELINE_CONNECTIONS_STRING="jdbc:hive2://${HIVESERVER2}/"
+
 if $SECURITY_HIVE; then
 	echo "Hive is secured"
 	kinit -kt $KRB_KEYTAB_HIVE $KRB_PRINCIPAL_HIVE
-
-	beeline -n $(whoami) -u "jdbc:hive2://${HIVESERVER2}/;principal=${KRB_PRINCIPAL_HIVE}${BTOPTS}" -e "CREATE TABLE test(id INT, name STRING) ROW FORMAT DELIMITED FIELDSTERMINATED BY ' ' STORED AS TEXTFILE;"
-	rc=$?
-	if [[ $rc != 0 ]]; then
-		echo "Create query failed! exiting"
-		echo " - Hive	- Failed [Create query failed]" >>./log/SummaryReport.txt
-		exit $rc
-	fi
-	echo "1 justin" >$HIVE_DATA_PATH
-	rc=$?
-	if [[ $rc != 0 ]]; then
-		echo "Input data generation failed! exiting"
-		echo " - Hive		- Failed [Input data generation failed]" >>./log/SummaryReport.txt
-		exit $rc
-	fi
-	echo "2 michael" >>$HIVE_DATA_PATH
-	rc=$?
-	if [[ $rc != 0 ]]; then
-		echo "Input data generation failed! exiting"
-		echo " - Hive		- Failed [Input data generation failed]" >>./log/SummaryReport.txt
-		exit $rc
-	fi
-	hdfs dfs -put $HIVE_DATA_PATH $HIVE_TABLE_LOC
-	rc=$?
-	if [[ $rc != 0 ]]; then
-		echo "Input data transfer failed! exiting"
-		echo " - Hive		- Failed [Input data transfer failed]" >>./log/SummaryReport.txt
-		exit $rc
-	fi
-	beeline -n $(whoami) -u "jdbc:hive2://${HIVESERVER2}/;principal=${KRB_PRINCIPAL_HIVE}${BTOPTS}" -e"SELECT * FROM test WHERE id=1;"
-	rc=$?
-	if [[ $rc != 0 ]]; then
-		echo "Select query failed! exiting"
-		echo " - Hive		- Failed [Select query failed]" >>./log/SummaryReport.txt
-		exit $rc
-	fi
-
-else
+	BEELINE_CONNECTIONS_STRING="${BEELINE_CONNECTIONS_STRING};principal=${KRB_PRINCIPAL_HIVE}${BTOPTS}"
+fi
 
 	echo "Hive is not secured"
-	beeline -n $(whoami) -u "jdbc:hive2://${HIVESERVER2}" -e "CREATE TABLE test(id INT, name STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '	' STORED AS TEXTFILE;"
+	beeline -n $(whoami) -u "${BEELINE_CONNECTIONS_STRING}" -e "CREATE TABLE test(id INT, name STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '	' STORED AS TEXTFILE;"
 	rc=$?
 	if [[ $rc != 0 ]]; then
 		echo "Create query failed! exiting"
@@ -86,7 +52,6 @@ else
 		exit $rc
 	fi
 
-fi
 
 if grep -f $HIVE_OUT $HIVE_DATA_PATH; then
 	echo "same data as in the output location"
