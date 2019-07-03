0d94efd75e157cf981ec7f5b83734aa4a5c30c78
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-03 14:34:16 -0700

    edit paths

--- SmokeTest/Startup.sh
+++ SmokeTest/Startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "">./log/SummaryReport.txt
+echo "">../log/SummaryReport.txt
 
 if $SECURITY ; then
 	kinit -kt $KRB_KEYTAB $KRB_PRINCIPAL
@@ -8,80 +8,80 @@ fi
 
 if $HDFS ; then
 	echo "Smoke test for HDFS"
-	bash ./bin/hdfsTest.sh
+	bash hdfsTest.sh
 
 fi
 
 if $MAPREDUCE ; then
 	echo "Smoke test for MAPREDUCE"
-	bash ./bin/mrTest.sh
+	bash mrTest.sh
 
 fi
 
 if $HIVE ; then
 	echo "Smoke test for HIVE"
-	bash ./bin/hiveTest.sh
+	bash hiveTest.sh
 
 fi
 
 if $HBASE ; then
 	echo "Smoke test for HBASE"
-	bash ./bin/hbaseTest.sh
+	bash hbaseTest.sh
 
 fi
 
 if $PIG ; then
 	echo "Smoke test for PIG"
-	bash ./bin/pigTest.sh
+	bash pigTest.sh
 fi
 
 if $IMPALA ; then
 	echo "Smoke test for IMPALA"
-	bash ./bin/impalaTest.sh
+	bash impalaTest.sh
 fi
 
 if $SPARK2 ; then
 	echo "Smoke test for SPARK"
-	bash ./bin/spark2Test.sh
+	bash spark2Test.sh
 fi
 
 if $SPARK ; then
 	echo "Smoke test for SPARK"
-	bash ./bin/sparkTest.sh
+	bash sparkTest.sh
 
 fi
 
 if $SOLR ; then
 	echo "Smoke test for SOLR"
-	bash ./bin/solrTest.sh
+	bash solrTest.sh
 
 fi
 
 if $KAFKA ; then
 	echo "Smoke test for KAFKA"
-	bash ./bin/kafkaTest.sh
+	bash kafkaTest.sh
 
 fi
 
 if $KUDU ; then
 	echo "Smoke test for KUDU"
-	bash ./bin/kuduTest.sh
+	bash kuduTest.sh
 
 fi
 
 
 if $KUDU_SPARK ; then
 	echo "Smoke test for KUDU_SPARK"
-	bash ./bin/kuduSparkTest.sh
+	bash kuduSparkTest.sh
 
 fi
 
 if $NIFI ; then
 	echo "Smoke test for NIFI"
-	bash ./bin/nifiTest.sh
+	bash nifiTest.sh
 
 fi
 
 
 echo "Get rid of all the test bits."
-bash ./bin/CleanItUp.sh
+bash CleanItUp.sh
