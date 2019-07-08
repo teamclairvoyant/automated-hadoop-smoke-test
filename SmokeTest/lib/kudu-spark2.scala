216587961e402dcb447700f6c6f525602703b8b1
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-08 15:14:21 -0700

    kudu spark test error handling

--- SmokeTest/lib/kudu-spark2.scala
+++ SmokeTest/lib/kudu-spark2.scala
@@ -22,16 +22,34 @@ kuduContext.createTable(
         .setNumReplicas(1)
         .addHashPartitions(List("number").asJava, 3))
 
+println("Table Exists: " + kuduContext.tableExists(kudu_table_name))
+
+if (kuduContext.tableExists(kudu_table_name) == false)
+{
+    println("Error creating table! Exiting!")
+    System.exit(1)
+}
+
 kuduContext.insertRows(df_create, kudu_table_name)
 
 val df_read = spark.sqlContext.read.options(Map("kudu.master" -> kuduContext.kuduMaster,"kudu.table" -> kudu_table_name)).kudu
 df_read.show()
 
-println("Table Exists: " + kuduContext.tableExists(kudu_table_name))
+println(df_read.count())
+if (df_read.count() != 3)
+{
+    println("Error inserting rows in the table! Exiting!")
+    System.exit(1)
+}
 
 println("Deleting Table!")
 kuduContext.deleteTable(kudu_table_name)
 
 println("Table Exists: " + kuduContext.tableExists(kudu_table_name))
+if (kuduContext.tableExists(kudu_table_name) == true)
+{
+    println("Error deleting table! Exiting!")
+    System.exit(1)
+}
 
-sys.exit
+System.exit(0)
