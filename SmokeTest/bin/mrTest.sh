f1118eb14eb6fd359dd9fb3a25e290fed9a0ff2e
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-09 11:35:22 -0700

    jar variable creation

--- SmokeTest/bin/mrTest.sh
+++ SmokeTest/bin/mrTest.sh
@@ -3,6 +3,8 @@ source ./conf/SmokeConfig.config
 
 echo "MAP_REDUCE_IN: $MAP_REDUCE_IN"
 echo "MAP_REDUCE_OUT: $MAP_REDUCE_OUT"
+echo "MAP_REDUCE_JAR: $MAP_REDUCE_JAR"
+
 
 hdfs dfs -rm -r -f $MAP_REDUCE_IN
 hdfs dfs -rm -r -f $MAP_REDUCE_OUT
@@ -11,9 +13,11 @@ hdfs dfs -mkdir -p $MAP_REDUCE_IN
 hdfs dfs -mkdir -p $MAP_REDUCE_OUT
 
 hdfs dfs -put -f ./lib/WordCountFile.txt $MAP_REDUCE_IN/
-rc=$?; if [[ $rc != 0 ]]; then echo "Input data tranfser failed! exiting"; echo " - MapReduce	- Failed [Input dat tranfser failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+rc=$?; if [[ $rc != 0 ]]; then echo "Input data tranfser failed! exiting"; echo " - MapReduce	- Failed [Input data tranfser failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-yarn jar ./lib/MapReduce-1.0.jar com.fs.WordCount $MAP_REDUCE_IN/WordCountFile.txt $MAP_REDUCE_OUT
+hdfs dfs -rm -r $MAP_REDUCE_OUT
+
+yarn jar $MAP_REDUCE_JAR wordcount $MAP_REDUCE_IN/WordCountFile.txt $MAP_REDUCE_OUT
 rc=$?; if [[ $rc != 0 ]]; then echo "Mapreduce Job failed! exiting"; echo " - MapReduce	- Failed [Wordcount test failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo "******************************************"
