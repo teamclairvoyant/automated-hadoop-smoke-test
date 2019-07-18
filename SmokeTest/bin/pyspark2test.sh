2c76ba2c04e485d0d715fe6d6a9a31b938354ecf
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-17 18:41:34 -0700

    pyspark2 tests

--- SmokeTest/bin/pyspark2test.sh
+++ SmokeTest/bin/pyspark2test.sh
@@ -6,7 +6,7 @@ spark-submit ./lib/piEstimation.py
 rc=$?; if [[ $rc != 0 ]]; then echo "Pi Estimation test failed! exiting"; echo " - pySpark2	- Failed [Pi Estimation test failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo "**************************************"
-echo "* pySpark test completed Successfully! *"
+echo "* pySpark2 test completed Successfully! *"
 echo "**************************************"
 
-echo " - pySpark	- Passed" >> ./log/SummaryReport.txt
+echo " - pySpark2	- Passed" >> ./log/SummaryReport.txt
