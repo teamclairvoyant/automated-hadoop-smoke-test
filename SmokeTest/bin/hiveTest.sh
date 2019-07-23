a6b6ee59b5c6df3e951587be56fcdd9a74da5b5a
Prakshal Jain <prakshaljain@Clairvoyants-MacBook-Pro.local>
2019-07-23 15:34:40 -0700

    echo line if else

--- SmokeTest/bin/hiveTest.sh
+++ SmokeTest/bin/hiveTest.sh
@@ -8,18 +8,21 @@ echo "HIVE_TABLE_NAME: $HIVE_TABLE_NAME"
 BEELINE_CONNECTIONS_STRING="jdbc:hive2://${HIVESERVER2}/"
 
 if $SECURITY; then
-	echo "Hive is secured"
+	echo "Hive is secured."
 	BEELINE_USER="hive"
 	echo "BEELINE_USER: $BEELINE_USER"
 	REALM=$(awk '/^ *default_realm/{print $3}' /etc/krb5.conf)
 	echo "REALM: $REALM"
 	PRINCIPAL="$BEELINE_USER"/_HOST@${REALM}
 	echo "PRINCIPAL: $PRINCIPAL"
-
+	
 	BEELINE_CONNECTIONS_STRING="${BEELINE_CONNECTIONS_STRING};principal=${PRINCIPAL}"
+
+else
+	echo "Hive is not secured."
 fi
 
-echo "Hive is not secured"
+
 beeline -n $(whoami) -u "${BEELINE_CONNECTIONS_STRING}" -e "CREATE TABLE ${HIVE_TABLE_NAME}(id INT, name STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '	' STORED AS TEXTFILE;"
 rc=$?
 if [[ $rc != 0 ]]; then
