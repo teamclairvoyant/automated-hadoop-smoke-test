c5d7c1809bded887e91d3a292a9f9f2e2d70dcf4
Michael Arnold <github@razorsedge.org>
2020-11-01 16:25:26 -0500

    Fix success/error checking for CDH6 HBase output.

--- SmokeTest/bin/hbaseTest.sh
+++ SmokeTest/bin/hbaseTest.sh
@@ -11,11 +11,11 @@ printf "list '%s'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1
 printf "list '%s'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1 | grep -q "[\"${HBASE_TABLE_NAME}\"]" 2>/dev/null
 rc=$?; if [[ $rc != 0 ]]; then echo "List command failed! exiting"; echo " - HBase        - Failed [List command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-printf "put '%s', 'row1', 'cf:a', 'value1'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1 |grep -q "0 row(s) in" 2>/dev/null
-rc=$?; if [[ $rc != 0 ]]; then echo "Put command failed! exiting"; echo " - HBase         - Failed [Put command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+printf "put '%s', 'row1', 'cf:a', 'value1'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1 | grep -q "ERROR: " 2>/dev/null
+rc=$?; if [[ $rc == 0 ]]; then echo "Put command failed! exiting"; echo " - HBase         - Failed [Put command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 printf "scan '%s'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1
-printf "scan '%s'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1 |grep -q "1 row(s) in" 2>/dev/null
+printf "scan '%s'\n" "$HBASE_TABLE_NAME" | hbase shell 2>&1 |grep -q "1 row(s)" 2>/dev/null
 rc=$?; if [[ $rc != 0 ]]; then echo "Scan command failed! exiting"; echo " - HBase        - Failed [Scan command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 
