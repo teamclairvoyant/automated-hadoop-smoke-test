b9bf900395b54c2baf5d3a80df8972025f9b9599
Michael Arnold <github@razorsedge.org>
2020-12-23 16:02:33 -0500

    Improvements to the Impala tests.

--- SmokeTest/bin/impalaTest.sh
+++ SmokeTest/bin/impalaTest.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 source ./conf/SmokeConfig.config
 
-echo "IMPALA_DAEMON: $IMPALA_DAEMON"
+echo "##########"
+#echo "IMPALA_DAEMON: $IMPALA_DAEMON"
+#echo "IMPALA_DATABASE_NAME: $IMPALA_DATABASE_NAME"
 echo "IMPALA_TABLE_NAME: $IMPALA_TABLE_NAME"
 echo "IMPALA_SSL_ENABLED: $IMPALA_SSL_ENABLED"
+echo "IMPALA_KERBEROS_ENABLED: $IMPALA_KERBEROS_ENABLED"
+echo "IMPALA_LDAP_ENABLED: $IMPALA_LDAP_ENABLED"
 
-IMPALA_CONNECT_STRING="${IMPALA_DAEMON}"
-if $IMPALA_SSL_ENABLED; then
-	echo "IKOPTS: ${IKOPTS}"
-	echo "ITOPTS: ${ITOPTS}"
-	IMPALA_CONNECT_STRING="${IMPALA_CONNECT_STRING} ${IKOPTS} ${ITOPTS}"
+IMPALA_CONNECT_STRING="--impalad=${IMPALA_DAEMON} --database=${IMPALA_DATABASE_NAME}"
+if [ "$IMPALA_SSL_ENABLED" == true ]; then
+#	echo "ITOPTS: ${ITOPTS}"
+	IMPALA_CONNECT_STRING="${IMPALA_CONNECT_STRING} ${ITOPTS}"
+fi
+if [ "$IMPALA_KERBEROS_ENABLED" == true ]; then
+#	echo "IKOPTS: ${IKOPTS}"
+	IMPALA_CONNECT_STRING="${IMPALA_CONNECT_STRING} ${IKOPTS}"
+fi
+if [ "$IMPALA_LDAP_ENABLED" == true ]; then
+#	echo "ILOPTS: ${ILOPTS}"
+	IMPALA_CONNECT_STRING="${IMPALA_CONNECT_STRING} ${ILOPTS}"
 fi
 echo "IMPALA_CONNECT_STRING: ${IMPALA_CONNECT_STRING}"
+echo "##########"
 
-#impala-shell -i $IMPALA_CONNECT_STRING -q "invalidate metadata;"
-#rc=$?; if [[ $rc != 0 ]]; then echo "Invalidation failed! exiting"; echo " - Impala       - Failed [Invalidation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
-
-impala-shell -i $IMPALA_CONNECT_STRING -d default -q "SET SYNC_DDL=true; CREATE TABLE ${IMPALA_TABLE_NAME} (x INT, y STRING);"
+exit
+impala-shell $IMPALA_CONNECT_STRING -q "SET SYNC_DDL=true; CREATE TABLE ${IMPALA_TABLE_NAME} (x INT, y STRING);"
 rc=$?; if [[ $rc != 0 ]]; then echo "Create query failed! exiting"; echo " - Impala       - Failed [Create query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-impala-shell -i $IMPALA_CONNECT_STRING -d default -q "INSERT INTO ${IMPALA_TABLE_NAME} VALUES (1, 'one'), (2, 'two'), (3, 'three');"
+impala-shell $IMPALA_CONNECT_STRING -q "INSERT INTO ${IMPALA_TABLE_NAME} VALUES (1, 'one'), (2, 'two'), (3, 'three');"
 rc=$?; if [[ $rc != 0 ]]; then echo "Insert query failed! exiting"; echo " - Impala       - Failed [Insert query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-impala-shell -i $IMPALA_CONNECT_STRING -d default -q "REFRESH ${IMPALA_TABLE_NAME}; SELECT * FROM ${IMPALA_TABLE_NAME};" --delimited --output_delimiter=, --output_file=impala_select_test.txt
+impala-shell $IMPALA_CONNECT_STRING -q "REFRESH ${IMPALA_TABLE_NAME}; SELECT * FROM ${IMPALA_TABLE_NAME};" --delimited --output_delimiter=, --output_file=impala_select_test.txt
 rc=$?; if [[ $rc != 0 ]]; then echo "Select query failed! exiting"; echo " - Impala       - Failed [Select query failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 cat <<EOF >impala_check.txt
@@ -36,14 +46,15 @@ status=$?
 
 if [[ $status = 0 ]]; then
 	echo "Files are the same"
-	echo "***************************************"
-	echo "* Impala test completed Successfully! *"
-	echo "***************************************"
+	echo "***************************"
+	echo "* Impala test successful! *"
+	echo "***************************"
 	echo " - Impala       - Passed" >> ./log/SummaryReport.txt
 else
 	echo "Files are different"
-	echo "***************************************"
-	echo "* Impala test not  completed Successfully! *"
-	echo "***************************************"
+	echo "***********************"
+	echo "* Impala test failed! *"
+	echo "***********************"
 	echo " - Impala       - Failed [Data in impala and Data inserted are different]" >> ./log/SummaryReport.txt
 fi
+
