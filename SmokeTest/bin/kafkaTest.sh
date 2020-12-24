509360168c0f3f00492cbd5cdd064f524991a650
Michael Arnold <github@razorsedge.org>
2020-12-24 15:37:43 -0500

    Unwrap if statements.

--- SmokeTest/bin/kafkaTest.sh
+++ SmokeTest/bin/kafkaTest.sh
@@ -9,7 +9,12 @@ echo "KAFKA_INP_LOC: $KAFKA_INP_LOC"
 echo "KAFKA_OUP_LOC: $KAFKA_OUP_LOC"
 
 kafka-topics  --zookeeper "${ZOOKEEPER}${ZOOKEEPER_ROOT}" --create --topic "$TOPIC_NAME" --partitions 1 --replication-factor 1
-rc=$?; if [[ $rc != 0 ]]; then echo "Cannot create Topic! exiting";  echo " - Kafka	- Failed [Cannot create Topic]" >> ./log/SummaryReport.txt; exit $rc; fi
+rc=$?
+if [[ $rc != 0 ]]; then
+  echo "Cannot create Topic! exiting"
+  echo " - Kafka	- Failed [Cannot create Topic]" >> ./log/SummaryReport.txt
+  exit $rc
+fi
 
 echo "Here Starts the producer...!!! "
 echo "$KAFKA_OUP_LOC" "$KAFKA_INP_LOC"
@@ -17,13 +22,24 @@ echo "$KAFKA_OUP_LOC" "$KAFKA_INP_LOC"
 echo "Please enter data ... cntrl+c for exit"
 
 kafka-console-producer --broker-list "$KAFKA_HOST" --topic "$TOPIC_NAME" < "$KAFKA_INP_LOC"
-rc=$?;echo "exitcode: $rc"; if [[ ($rc != 0) && ($rc != 130) ]]; then echo "Cannot produce data! exiting";  echo " - Kafka		- Failed [Cannot produce data]" >> ./log/SummaryReport.txt; exit $rc; fi
+rc=$?
+echo "exitcode: $rc"
+if [[ ($rc != 0) && ($rc != 130) ]]; then
+  echo "Cannot produce data! exiting"
+  echo " - Kafka		- Failed [Cannot produce data]" >> ./log/SummaryReport.txt
+  exit $rc
+fi
 
 echo "Here Starts the Consuming...!!! "
 echo "Check log for  Data... cntrl+c for exit"
 
 kafka-console-consumer --bootstrap-server "$KAFKA_HOST" --topic "$TOPIC_NAME" --from-beginning > "$KAFKA_OUP_LOC"
-rc=$?; if [[ ($rc != 0) && ($rc != 130) ]]; then echo "Cannot consume data! exiting";  echo " - Kafka		- Failed [Cannot consume data]" >> ./log/SummaryReport.txt; exit $rc; fi
+rc=$?
+if [[ ($rc != 0) && ($rc != 130) ]]; then
+  echo "Cannot consume data! exiting"
+  echo " - Kafka		- Failed [Cannot consume data]" >> ./log/SummaryReport.txt
+  exit $rc
+fi
 
 if grep -f "$KAFKA_OUP_LOC" "$KAFKA_INP_LOC"
 then
