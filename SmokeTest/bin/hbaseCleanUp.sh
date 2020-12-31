31b15ca3ca6af5397bed0e2c101be67204042471
Michael Arnold <github@razorsedge.org>
2020-12-30 23:07:38 -0500

    Fix typo.

--- SmokeTest/bin/hbaseCleanUp.sh
+++ SmokeTest/bin/hbaseCleanUp.sh
@@ -3,5 +3,5 @@ source ./conf/SmokeConfig.config
 
 echo "HBASE_TABLE_NAME: $HBASE_TABLE_NAME"
 
-printf "disable '%s'\ndropp '%s'\n" "$HBASE_TABLE_NAME" "$HBASE_TABLE_NAME" | hbase shell 2>&1
+printf "disable '%s'\ndrop '%s'\n" "$HBASE_TABLE_NAME" "$HBASE_TABLE_NAME" | hbase shell 2>&1
 
