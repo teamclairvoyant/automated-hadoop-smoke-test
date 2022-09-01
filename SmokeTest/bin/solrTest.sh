6c29e80f9e26680e61ff07eb82572d948c43c3c4
Michael Arnold <github@razorsedge.org>
2022-09-01 09:15:35 -0400

    Update Solr test to work in a Kerberos+TLS environment.

--- SmokeTest/bin/solrTest.sh
+++ SmokeTest/bin/solrTest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#set -x
 
 source ./conf/SmokeConfig.config
 
 echo "SOLR_SERVER: $SOLR_SERVER"
+#echo "SOLR_OPS: $SOLR_OPS"
 echo "SOLR_CURL_OPS: $SOLR_CURL_OPS"
 echo "SOLR_SSL_ENABLED: $SOLR_SSL_ENABLED"
 echo "SOLR_EXAMPLE_HOME: $SOLR_EXAMPLE_HOME"
@@ -20,19 +22,29 @@ rc=$?; if [[ $rc != 0 ]]; then echo "Instance directory generation failed! exiti
 
 solrctl instancedir --create "$SOLR_INSTANTDIR_NAME" /tmp/solr.$$
 rc=$?; if [[ $rc != 0 ]]; then echo "Config file creation failed! exiting"; echo " - Solr         - Failed [Config file creation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+#solrctl instancedir --list | grep "^${SOLR_INSTANTDIR_NAME}$"
 
 solrctl collection --create "$SOLR_COLLECTION_NAME" -s 1 -c "$SOLR_INSTANTDIR_NAME"
 rc=$?; if [[ $rc != 0 ]]; then echo "Collection creation failed! exiting"; echo " - Solr         - Failed [Collection creation failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+#solrctl collection --list | grep "^${SOLR_COLLECTION_NAME} "
 
-java -Durl="${SOLR_PROTOCOL}"://"$SOLR_SERVER"/solr/"$SOLR_COLLECTION_NAME"/update -jar "$SOLR_EXAMPLE_HOME"/exampledocs/post.jar "$SOLR_EXAMPLE_HOME"/exampledocs/*.xml
-rc=$?; if [[ $rc != 0 ]]; then echo "durl command failed! exiting"; echo " - Solr         -  Failed [durl command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+echo "* Uploading data..."
+#java $SOLR_OPS -Durl="${SOLR_PROTOCOL}"://"$SOLR_SERVER"/solr/"$SOLR_COLLECTION_NAME"/update -jar ${SOLR_EXAMPLE_HOME}/exampledocs/post.jar ${SOLR_EXAMPLE_HOME}/exampledocs/*.xml
+#/opt/cloudera/parcels/CDH/lib/solr/bin/post -url "${SOLR_PROTOCOL}"://"$SOLR_SERVER"/solr/"$SOLR_COLLECTION_NAME"/update ${SOLR_EXAMPLE_HOME}/exampledocs/*.xml
+#rc=$?; if [[ $rc != 0 ]]; then echo "java command failed! exiting"; echo " - Solr         -  Failed [java command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+#curl -s $SOLR_CURL_OPS "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}/update?commit=true" -H "Content-Type: application/xml" --data-binary @${SOLR_EXAMPLE_HOME}/exampledocs/*.xml
+curl -s $SOLR_CURL_OPS "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}/update?commit=true" -H "Content-Type: text/csv" --data-binary @`eval echo ${SOLR_EXAMPLE_HOME}/exampledocs/books.csv`
+rc=$?; if [[ $rc != 0 ]]; then echo "curl upload command failed! exiting"; echo " - Solr         -  Failed [curl upload command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
-curl "$SKOPTS" "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}_shard1_replica1/select?q=*%3A*&wt=json&indent=true"
-rc=$?; if [[ $rc != 0 ]]; then echo "curl command failed! exiting"; echo " - Solr         -  Failed [curl command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
+echo "* Querying..."
+#curl -s $SOLR_CURL_OPS "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}/get?id=0812550706"
+curl -s $SOLR_CURL_OPS "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}/query?q=series_t:Chronicles&fl=name,author"
+#curl -s $SOLR_CURL_OPS "${SOLR_PROTOCOL}://${SOLR_SERVER}/solr/${SOLR_COLLECTION_NAME}/select?q=*%3A*&wt=json&indent=true"
+rc=$?; if [[ $rc != 0 ]]; then echo "curl query command failed! exiting"; echo " - Solr         -  Failed [curl query command failed]" >> ./log/SummaryReport.txt; exit $rc; fi
 
 echo ""
-echo "***************************************"
-echo "*  Solr test completed Successfully ! *"
-echo "***************************************"
+echo "*************************************"
+echo "* Solr test completed Successfully! *"
+echo "*************************************"
 
 echo " - Solr         - Passed" >> ./log/SummaryReport.txt
