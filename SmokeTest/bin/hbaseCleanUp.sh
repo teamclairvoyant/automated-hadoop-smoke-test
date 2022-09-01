0c8c5a3b57b1e27485bbe8d0af4eedef760b3f17
Michael Arnold <github@razorsedge.org>
2022-09-01 09:55:42 -0400

    Slight improvements to the HBase test and cleanup.

--- SmokeTest/bin/hbaseCleanUp.sh
+++ SmokeTest/bin/hbaseCleanUp.sh
@@ -4,4 +4,30 @@ source ./conf/SmokeConfig.config
 echo "HBASE_TABLE_NAME: $HBASE_TABLE_NAME"
 
 printf "disable '%s'\ndrop '%s'\n" "$HBASE_TABLE_NAME" "$HBASE_TABLE_NAME" | hbase shell 2>&1
+rc=$?
+if [[ $rc != 0 ]]; then
+  echo "Disable/Drop command failed! exiting"
+  #echo " - HBase        - Failed [Disable/Drop command failed]" >> ./log/SummaryReport.txt
+  exit $rc
+fi
+
+##printf "disable '%s'" "$HBASE_TABLE_NAME" | hbase shell 2>&1 | grep -q "ERROR: Table ${HBASE_TABLE_NAME}" 2>/dev/null
+#printf "disable '%s'" "$HBASE_TABLE_NAME" | hbase shell
+#rc=$?
+#if [[ $rc != 0 ]]; then
+#  echo "Disable command failed! exiting"
+#  echo " - HBase        - Failed [Disable command failed]" >> ./log/SummaryReport.txt
+#  exit $rc
+#fi
+#
+##printf "drop '%s'" "$HBASE_TABLE_NAME" | hbase shell 2>&1 | grep -q "ERROR: Table ${HBASE_TABLE_NAME}" 2>/dev/null
+#printf "drop '%s'" "$HBASE_TABLE_NAME" | hbase shell
+#rc=$?
+#if [[ $rc != 0 ]]; then
+#  echo "Drop command failed! exiting"
+#  echo " - HBase        - Failed [Drop command failed]" >> ./log/SummaryReport.txt
+#  exit $rc
+#fi
+
+echo "HBase ${HBASE_TABLE_NAME} table removed!"
 
