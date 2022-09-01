e9f94c2661f45ae66300ca06b16330c56c91386a
Michael Arnold <github@razorsedge.org>
2022-09-01 10:41:42 -0400

    Switch the ./conf/kafka.conf based on $KAFKA_SECURITY_TYPE.

--- SmokeTest/bin/kafkaCleanUp.sh
+++ SmokeTest/bin/kafkaCleanUp.sh
@@ -7,7 +7,7 @@ rm -r -f "$KAFKA_OUP_LOC"
 export KAFKA_OPTS="-Dlog4j.configuration=file:./conf/tools-log4j.properties $KAFKA_OPTS"
 #export KAFKA_OPTS="-Djava.security.auth.login.config=./conf/jaas-client.conf $KAFKA_OPTS"
 
-_KAFKA_TOPIC_OPTS="--command-config=./conf/kafka.conf"
+_KAFKA_TOPIC_OPTS="--command-config=./conf/kafka.conf-${KAFKA_SECURITY_TYPE}"
 
 #kafka-topics  --zookeeper "$KAFKA_ZOOKEEPER" --delete --topic "$TOPIC_NAME"
 kafka-topics $_KAFKA_TOPIC_OPTS --bootstrap-server "$KAFKA_HOST" --delete --topic "$TOPIC_NAME"
