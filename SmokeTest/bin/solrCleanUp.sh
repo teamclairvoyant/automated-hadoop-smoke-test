c871633c520ab01b713f8970156c49309190cbf1
Michael Arnold <github@razorsedge.org>
2022-09-07 11:12:59 -0400

    Do not exit on cleanup errors.

--- SmokeTest/bin/solrCleanUp.sh
+++ SmokeTest/bin/solrCleanUp.sh
@@ -6,10 +6,10 @@ echo "SOLR_COLLECTION_NAME: $SOLR_COLLECTION_NAME"
 echo "SOLR_INSTANTDIR_NAME: $SOLR_INSTANTDIR_NAME"
 
 solrctl collection --delete "$SOLR_COLLECTION_NAME"
-rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
+#rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
 
 solrctl instancedir --delete "$SOLR_INSTANTDIR_NAME"
-rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
+#rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
 
 rm -rf /tmp/"$SOLR_INSTANTDIR_NAME".$$
-rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
+#rc=$?; if [[ $rc != 0 ]]; then echo "error! exiting"; exit $rc; fi
